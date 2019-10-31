f993ddc40c391a640a6cda302697d5a25be19690
Chris Basham <chrisdbasham0317@gmail.com>
2019-10-31 12:12:00 -0600

    Setup testing/redux dependencies

--- src/App.js
+++ src/App.js
@@ -4,26 +4,12 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      isLoading: true,
-      img: ''
-    }
-  }
 
-  // componentDidMount = async () => {
-  //   await fetch('https://www.superherodb.com/pictures2/portraits/10/100/791.jpg')
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       return this.setState({
-  //         isLoading: false,
-  //         img: data
-  //       })
-  //     })
-  // }
+  }
 
   render() {
     return (
-      <img src='https://www.superherodb.com/pictures2/portraits/10/100/791.jpg' />
+      <h1>Hello</h1>
     )
 }
 }
