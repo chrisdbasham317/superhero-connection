f84d339fcc2bb9016c73d018fa218ee65f619ae3
Chris Basham <chrisdbasham0317@gmail.com>
2019-10-31 11:53:26 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+      img: ''
+    }
+  }
+
+  // componentDidMount = async () => {
+  //   await fetch('https://www.superherodb.com/pictures2/portraits/10/100/791.jpg')
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       return this.setState({
+  //         isLoading: false,
+  //         img: data
+  //       })
+  //     })
+  // }
+
+  render() {
+    return (
+      <img src='https://www.superherodb.com/pictures2/portraits/10/100/791.jpg' />
+    )
+}
 }
 
 export default App;
