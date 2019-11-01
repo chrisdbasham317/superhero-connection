3ce6d76125bf40c2a879c11f278151c559cd3ace
Chris Basham <chrisdbasham0317@gmail.com>
2019-11-01 11:39:56 -0600

    Add additional hero info to render

--- src/Containers/HeroPage/HeroPage.js
+++ src/Containers/HeroPage/HeroPage.js
@@ -7,6 +7,9 @@ import loadingIcon from '../../images/loading-icon.jpg';
 const HeroPage = ({ id, heroes }) => {
   const heroId = parseInt(id.split('').filter(elem => elem !== '}').join(''));
   const currentHero = heroes.filter(hero => hero.id === heroId)[0];
+  const attributes = currentHero && Object.keys(currentHero.powerstats);
+  const aliases = currentHero && currentHero.biography.aliases.map(alias => <li>{alias}</li>);
+  const powerStats = currentHero ? attributes.map(stat => <li>{`${stat}: ${currentHero.powerstats[stat]}`}</li>) : null;
   return (
     <section>
       {!currentHero && loadingIcon}
@@ -14,6 +17,18 @@ const HeroPage = ({ id, heroes }) => {
       <div className='div--loaded-hero-data-render'>
         <h1>{currentHero.name}</h1>
         <img src={currentHero.images.md} />
+        <ul>
+          <p>Aliases</p>
+          {aliases}
+        </ul>
+        <ul>
+          <p>Attributes</p>
+          {powerStats}
+        </ul>
+        <p>Birth Place: {currentHero.biography.placeOfBirth}</p>
+        <p>First Appearance: {currentHero.biography.firstAppearance}</p>
+        <p>Publisher: {currentHero.biography.publisher}</p>
+        <p>Alignment: {currentHero.biography.alignment}</p>
       </div>    
       }
     </section>
