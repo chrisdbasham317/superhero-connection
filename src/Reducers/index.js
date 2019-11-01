dc5255b6d7d4e8d95dad953a56aa4bddb17927bc
Chris Basham <chrisdbasham0317@gmail.com>
2019-10-31 19:52:48 -0600

    Add heroes, isloading, error, and winner to root reducer

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux';
-
-
+import { heroes } from './heroes';
+import { isLoading } from './isLoading';
+import { error } from './error';
+import { winner } from './winner';
+ 
 const rootReducer = combineReducers({
-
+  heroes,
+  isLoading,
+  error,
+  winner
 });
 
 
