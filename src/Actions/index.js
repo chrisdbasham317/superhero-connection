a620d1bbf9f69a58b39acbd052420e77ee103856
Chris Basham <chrisdbasham0317@gmail.com>
2019-10-31 19:43:52 -0600

    Add heroes reducer

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,14 +1,24 @@
 export const isLoading = bool => ({
   type: 'IS_LOADING',
-  bool
+  isLoading: bool
 });
 
 export const hasErrored = msg => ({
   type: 'HAS_ERRORED',
-  msg
+  error: msg
 });
 
 export const setHeroes = heroes => ({
   type: 'SET_HEROES',
   heroes
+});
+
+export const setWinner = id => ({
+  type: 'SET_WINNER',
+  winnerId: id
+});
+
+export const setModal = bool => ({
+  type: 'SET_MODAL',
+  bool
 })
