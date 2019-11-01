39334f20743e371b968761c3c8b00bede31049d3
Chris Basham <chrisdbasham0317@gmail.com>
2019-11-01 11:46:03 -0600

    Add battle button to bottom of hero page

--- src/Containers/HeroPage/HeroPage.js
+++ src/Containers/HeroPage/HeroPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import loadingIcon from '../../images/loading-icon.jpg';
@@ -7,21 +8,21 @@ import loadingIcon from '../../images/loading-icon.jpg';
 const HeroPage = ({ id, heroes }) => {
   const heroId = parseInt(id.split('').filter(elem => elem !== '}').join(''));
   const currentHero = heroes.filter(hero => hero.id === heroId)[0];
-  const attributes = currentHero && Object.keys(currentHero.powerstats);
-  const aliases = currentHero && currentHero.biography.aliases.map(alias => <li>{alias}</li>);
+  const attributes = currentHero ? Object.keys(currentHero.powerstats) : null;
+  const aliases = currentHero ? currentHero.biography.aliases.map(alias => <li>{alias}</li>) : null;
   const powerStats = currentHero ? attributes.map(stat => <li>{`${stat}: ${currentHero.powerstats[stat]}`}</li>) : null;
   return (
-    <section>
+    <section className='section--hero-page'>
       {!currentHero && loadingIcon}
       {currentHero && 
       <div className='div--loaded-hero-data-render'>
-        <h1>{currentHero.name}</h1>
-        <img src={currentHero.images.md} />
-        <ul>
+        <h1 className='h1--hero-name'>{currentHero.name}</h1>
+        <img src={currentHero.images.md} alt={`${currentHero.name} photo`} />
+        <ul className='ul--alias-list'>
           <p>Aliases</p>
           {aliases}
         </ul>
-        <ul>
+        <ul className='ul--stats-list'>
           <p>Attributes</p>
           {powerStats}
         </ul>
@@ -29,6 +30,9 @@ const HeroPage = ({ id, heroes }) => {
         <p>First Appearance: {currentHero.biography.firstAppearance}</p>
         <p>Publisher: {currentHero.biography.publisher}</p>
         <p>Alignment: {currentHero.biography.alignment}</p>
+        <div className='link--hero-battle-btn'>
+          <Link to='/battle'>Choose Hero</Link>
+        </div>
       </div>    
       }
     </section>
