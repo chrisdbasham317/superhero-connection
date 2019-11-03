53ae2064bfc017eedea586fe68f245fe1e6ad7b5
Chris Basham <chrisdbasham0317@gmail.com>
2019-11-03 15:44:25 -0700

    Add welcome info modal to root path

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactModal from 'react-modal';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Route, NavLink } from 'react-router-dom';
@@ -12,16 +13,52 @@ import './App.css';
 export class App extends Component {
   constructor() {
     super();
-
+    this.state = {
+      modalState: true
+    }
   }
 
   componentDidMount() {
     this.props.fetchHeroes();
   }
 
+  handleClick = event => {
+    event.preventDefault();
+    this.setState({ modalState: false });
+  }
+
   render() {
     return (
       <div className='div--app'>
+        <ReactModal
+          ariaHideApp={false}
+          isOpen={this.state.modalState}
+          style={{
+            overlay: {
+              position: "fixed",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              backgroundColor: "rgba(0, 0, 0, 0.85)",
+            },
+            content: {}
+          }}
+          contentLabel="App Welcome Page"
+          className="WelcomeModal"
+          overlayClassName="WelcomeOverlay"
+        >
+          <article className='article--welcome-modal'>
+            <h1 className='h1--welcome-modal'>Welcome to SuperHero Battle!</h1>
+            <p className='p--welocome-modal'>
+              <span>SuperHero Battle</span> is a place where you can find out more information about some of your favorite superheroes and villians. More importantly, it's where you find out who could beat who in a head to head fight!
+            </p>
+            <button
+              className='button--close-modal'
+              onClick={(event) => this.handleClick(event)}
+            >Enter to find out!</button>
+          </article>
+        </ReactModal>
         <header className='header--nav-bar'>
           <h1 className='h1--main-title'>SuperHero Battle</h1>
           <div className='div--nav-links'>
