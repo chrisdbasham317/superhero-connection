fc2b1cb15afed91521eb39fb16ff2a8381cbabbd
Chris Basham <chrisdbasham0317@gmail.com>
2019-11-03 14:38:42 -0700

    Add logic for instances of a tie

--- src/Containers/WinnerCircle/WinnerCircle.js
+++ src/Containers/WinnerCircle/WinnerCircle.js
@@ -4,14 +4,17 @@ import './WinnerCircle.css'
 
 export const WinnerCircle = ({winner, heroes}) => {
   const winnerInfo = heroes.filter(hero => hero.name === winner)[0]
-  console.log(winnerInfo)
   return (
     <section className='section--winner-circle'>
-      <h1 className='h1--winner-circle'>WinnerCircle</h1>
-      <div className='div--winner-info'>
-        <h2 className='h2--winner-name'>{winnerInfo.name}</h2>
-        <img className='img--winner-img'src={winnerInfo.images.md} />
-      </div>
+    {!winner && <h1>It seems those heroes are evenly matched</h1>}
+      {winner &&
+      <section className='section--winner-circle'>
+        <h1 className='h1--winner-circle'>WinnerCircle</h1>
+        <div className='div--winner-info'>
+          <h2 className='h2--winner-name'>{winnerInfo.name}</h2>
+          <img className='img--winner-img'src={winnerInfo.images.md} />
+        </div>
+      </section>}
     </section>
   )
 }
