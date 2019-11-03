a338a99dfb59c7ce1518532a27601e17526201b0
Chris Basham <chrisdbasham0317@gmail.com>
2019-11-03 14:03:14 -0700

    Add hero info into the winner page

--- src/Containers/WinnerCircle/WinnerCircle.js
+++ src/Containers/WinnerCircle/WinnerCircle.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 
-const WinnerCircle = () => {
+export const WinnerCircle = ({winner, heroes}) => {
+  const winnerInfo = heroes.filter(hero => hero.name === winner)[0]
+  console.log(winnerInfo)
   return (
-    <h1>WinnerCircle</h1>
+    <section>
+      <h1>WinnerCircle</h1>
+      <h2>{winnerInfo.name}</h2>
+      <img src={winnerInfo.images.md}/>
+    </section>
   )
 }
 
-export default WinnerCircle;
+export const mapStateToProps = ({winner, heroes}) => ({
+  winner,
+  heroes
+})
+
+export default connect(mapStateToProps)(WinnerCircle);
