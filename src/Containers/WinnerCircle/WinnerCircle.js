5693c94391b3b18a3c2c951376fb6a03ef656fab
Chris Basham <chrisdbasham0317@gmail.com>
2019-11-03 14:32:52 -0700

    Finish styling for winner display

--- src/Containers/WinnerCircle/WinnerCircle.js
+++ src/Containers/WinnerCircle/WinnerCircle.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import './WinnerCircle.css'
 
 export const WinnerCircle = ({winner, heroes}) => {
   const winnerInfo = heroes.filter(hero => hero.name === winner)[0]
   console.log(winnerInfo)
   return (
-    <section>
-      <h1>WinnerCircle</h1>
-      <h2>{winnerInfo.name}</h2>
-      <img src={winnerInfo.images.md}/>
+    <section className='section--winner-circle'>
+      <h1 className='h1--winner-circle'>WinnerCircle</h1>
+      <div className='div--winner-info'>
+        <h2 className='h2--winner-name'>{winnerInfo.name}</h2>
+        <img className='img--winner-img'src={winnerInfo.images.md} />
+      </div>
     </section>
   )
 }
