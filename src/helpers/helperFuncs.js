7ddd51745951bd6afa3611d2542378e543e4d4fc
Chris Basham <chrisdbasham0317@gmail.com>
2019-11-04 10:50:45 -0700

    Add test for getting default heroes helper fn

--- src/helpers/helperFuncs.js
+++ src/helpers/helperFuncs.js
@@ -1,5 +1,4 @@
 export const getDefaultHeroes = (heroes) => {
-  console.log(heroes);
   const defaultHeroIds = [38, 60, 63, 70, 76, 93, 95, 98, 204, 230, 459, 413, 387, 265, 289, 298, 306, 579, 601, 632, 644, 642, 686];
   const defaultHeroes = defaultHeroIds.map(id => {
     return heroes.reduce((acc, currentHero) => {
